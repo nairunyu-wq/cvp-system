38cb7ac9bffcff108ace1cd2f50ef6a0fb83e435
nairunyu-wq <nairunyu@gmail.com>
2026-04-19 19:12:58 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,9 @@ export default defineConfig(({mode}) => {
         '@': path.resolve(__dirname, 'src'),
       },
     },
+    css: {
+      postcss: './postcss.config.js',
+    },
     build: {
       outDir: 'dist',
       assetsDir: 'assets',
